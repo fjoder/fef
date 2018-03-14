6c32cea1dd2214e2dd1d4ace1e4b044256b98369
Franklin Oder <fjoder@gmail.com>
2018-03-13 23:20:32 -0400

    Completed lesson 8

--- js/lesson8.js
+++ js/lesson8.js
@@ -2,9 +2,10 @@ function createGallery() {
   const dir = "../html/imgs/logos/";
   const gallery = document.getElementById('gallery');
 
-  for (let i = 0; i < 10; i++) {
-    let element = document.createElement("img");
-    element.setAttribute("src", "00" + i + ".svg");
+  for (let i = 1; i <= 10; i++) {
+    const element = document.createElement("img");
+    let url = dir + i + ".svg";
+    element.setAttribute("src", url);
     gallery.appendChild(element);
   }
 }
